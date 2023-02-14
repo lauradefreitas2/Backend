86337052a1d6829913cc79d1acf9f3962a26d672
lauradefreitas2 <lauradovalegameplays@gmail.com>
2023-02-14 09:31:13 -0300

    add os ex de ontem

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Figura.Circulo circulo = new Figura.Circulo(5.0);
-        System.out.println("Perímetro do círculo: " + circulo.calcularPerimetro());
-
-        Figura.Quadrado quadrado = new Figura.Quadrado(6.0);
-        System.out.println("Perímetro do quadrado: " + quadrado.calcularPerimetro());
-    }
-}
